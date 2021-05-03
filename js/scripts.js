265f9a83a20b49ea9ea52b038004495807bc6d93
Kolby Kruger <kolbykruger@gmail.com>
2021-05-03 19:03:54 -0400

    initial blocking

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     triggerHook: 1,
                     duration: '300%',
                 })
+                    .setTween(TweenMax.from(word, 1, { y: iconDist, ease: Power3.easeNone }))
                     .setTween(TweenMax.to(word, 1, { y: wordDist, ease: Power3.easeNone }))
                     //.setTween(TweenMax.to(icon, 1, { y: iconDist, ease: Power3.easeNone }))
                     .addTo(controller)
